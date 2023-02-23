e84e149755ab94b00991b432ce1da243673fa9f6
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-24 00:06:39 +0530

    Firebase Added Successfully

--- my_cart/src/App.js
+++ my_cart/src/App.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import Cart from './Cart';
 import NavBar from './NavBar';
-
+import "firebase/database";
+import firebase from "firebase/compat/app"
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [{
-        price: 1299,
-        qty: 1,
-        img: 'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        title: "Watch",
-        id: 1
-      }, {
-        price: 799,
-        qty: 1,
-        img: 'https://images.unsplash.com/photo-1566150905968-62f0de3d6df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhhbmRiYWd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-        title: "Handbag",
-        id: 2
-      }, {
-        price: 49999,
-        qty: 1,
-        img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-        title: "Laptop",
-        id: 3
-      }
-      ]
-    }
+      products: []
+    };
+    this.db = firebase.firestore();
   }
+
+  componentDidMount() {
+    this.db.collection("products").onSnapshot(snapshot => {
+      // console.log(snapshot)
+      const products = snapshot.docs.map(doc => {
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
+      });
+      this.setState({
+        products : products
+      })
+    });
+  }
+
   handleIncreaseQuantity = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
@@ -73,7 +71,8 @@ class App extends React.Component {
     const { products } = this.state;
     let price = 0;
     products.map((product) => {
-      price = price + product.qty*product.price;
+      price = price + product.qty * product.price;
+      return '';
     });
     return price;
   }
@@ -89,10 +88,9 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteQuantity={this.handleDeleteItem}
         />
-        <div style={{fontSize:20}}>Total : {this.getCartTotalPrice()}</div>
+        <div style={{ fontSize: 20 }}>Total : {this.getCartTotalPrice()}</div>
       </div>
     );
   }
 }
-
 export default App;
--- my_cart/src/index.js
+++ my_cart/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  
+import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB66O8ifqNde97Sy9Tx66o4YGiZz0yDd3k",
+  authDomain: "mycart-d4c9e.firebaseapp.com",
+  projectId: "mycart-d4c9e",
+  storageBucket: "mycart-d4c9e.appspot.com",
+  messagingSenderId: "798104108976",
+  appId: "1:798104108976:web:80a52e379560dcd127e075"
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+firebase.initializeApp(firebaseConfig);
